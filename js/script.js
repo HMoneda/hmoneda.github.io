bde60370925879895ab4f8872dcc80181eb6a686
HMoneda <heinze.moneda@gmail.com>
2024-04-29 00:51:28 +0800

    Home page done

--- js/script.js
+++ js/script.js
@@ -1,26 +1,5 @@
-window.onload = () => {
-  const menu = document.getElementById('mobile-nav-menu');
-  const close = document.getElementById('mobile-nav-close');
-
-  function showMobileNav() {
-    const nav = document.getElementById('mobile-nav');
-
-    if (!nav.classList.contains('mobile-nav-active')) {
-      nav.classList.add('mobile-nav-active');
-      close.classList.add('mobile-nav-active');
-      menu.classList.add('mobile-nav-inactive');
-    } else {
-      nav.classList.remove('mobile-nav-active');
-      close.classList.remove('mobile-nav-active');
-      menu.classList.remove('mobile-nav-inactive');
-    }
-  }
-
-  menu.addEventListener('click', (e) => {
-    showMobileNav();
-  });
-
-  close.addEventListener('click', (e) => {
-    showMobileNav();
-  });
-};
+const menu = document.getElementById('hamburger-icon');
+const navMenu = document.querySelector('.nav-menu');
+menu.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
